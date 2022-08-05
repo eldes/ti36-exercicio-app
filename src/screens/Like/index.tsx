99a2c2fd48d2715221eea5bf4c6cd4505de7fa1f
Eldes <github@eldes.com>
2022-08-05 20:43:55 -0300

    [+] Implement like action

--- src/screens/Like/index.tsx
+++ src/screens/Like/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Image, Pressable, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import fotoImage from '../../../assets/like/foto.jpg';
 import styles from './styles';
 
@@ -6,12 +8,36 @@ type Props = {
 };
 
 const LikeScreen: React.FC<Props> = function (props) {
+  const [liked, setLiked] = useState(false);
+
+  let firstPressTime = new Date().getTime();
+
+  const imagePressed = function () {
+    const secondPressTime = new Date().getTime();
+    console.log(secondPressTime - firstPressTime);
+    
+    if ((secondPressTime - firstPressTime) <= 200) {
+      setLiked(!liked);
+    }
+    firstPressTime = secondPressTime;
+  };
+
+  const likeButtonPressed = function () {
+    setLiked(!liked);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.post}>
-        <Image style={styles.postImage} source={fotoImage} />
-        <TouchableOpacity style={styles.postLikeButton}>
-          Like
+        <Pressable onPress={imagePressed}>
+          <Image style={styles.postImage} source={fotoImage} />
+        </Pressable>
+        <TouchableOpacity style={styles.postLikeButton} onPress={likeButtonPressed}>
+          {(liked) ? (
+            <Icon name="favorite" size={24} color="#999" />
+          ) : (
+            <Icon name="favorite-outline" size={24} color="#999" />
+          )}
         </TouchableOpacity>
       </View>
     </View>
--- src/screens/Like/styles.ts
+++ src/screens/Like/styles.ts
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
   },
 
   postLikeButton: {
-    
+    paddingVertical: 8,
   },
 });
 
