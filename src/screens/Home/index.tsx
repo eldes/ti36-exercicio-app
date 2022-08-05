8e0f2c1dc60b4ef217305299772bcd2f7a82ba59
Eldes <eldes.poliveira@sp.senac.br>
2022-08-04 21:29:17 -0300

    [+] Home Screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,36 @@
-const HomeScreen: React.FC = function () {
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Pressable, Text, View } from 'react-native';
+import { StackParams } from '../../../App';
+import styles from './styles';
+
+type Props = NativeStackScreenProps<StackParams, 'Home'>;
+
+type ButtonData = {
+  text: string;
+  to: keyof StackParams;
+};
+
+const buttonDataList: ButtonData[] = [
+  {
+    text: 'Like',
+    to: 'Like',
+  },
+  {
+    text: 'Cadastro',
+    to: 'Cadastro',
+  },
+];
+
+const HomeScreen: React.FC<Props> = function (props) {
+
   return (
-    <></>
+    <View style={styles.home}>
+      {buttonDataList.map( buttonData =>
+        <Pressable onPress={() => props.navigation.navigate(buttonData.to)}>  
+          <Text style={styles.button}>{buttonData.text}</Text>
+        </Pressable>  
+      )}
+    </View>
   );
 };
 
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  home: {
+    flexGrow: 1,
+    backgroundColor: '#eee',
+    padding: 16,
+  },
+
+  button: {
+    marginVertical: 8,
+    backgroundColor: '#fff',
+    color: '#999',
+    padding: 8,
+    borderRadius: 4,
+  },
+});
+
+export default styles;
