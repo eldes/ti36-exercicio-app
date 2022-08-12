6a44abc282755d8192ce2850882551f44d54b5b3
Eldes <github@eldes.com>
2022-08-11 21:41:14 -0300

    [+] Add Drawer

--- App.tsx
+++ App.tsx
@@ -1,50 +1,28 @@
-import { AppBar, IconButton } from '@react-native-material/core';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator, NativeStackHeaderProps } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { Platform } from 'react-native';
-import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import ChatScreen from './src/screens/Chat';
 import HomeScreen from './src/screens/Home';
 import LikeScreen from './src/screens/Like';
 import RatingScreen from './src/screens/Rating';
 import TaskListScreen from './src/screens/TaskList';
 
-const Header: React.FC<NativeStackHeaderProps> = ({ route, back, navigation }) => {
-  const insets = useSafeAreaInsets();
-  return (
-    <AppBar
-      title={route.name}
-      leading={
-        back &&
-        (props => (
-          <IconButton
-            icon={props => <Icon name={Platform.OS === "ios" ? "chevron-left" : "arrow-left"} {...props} />}
-            onPress={navigation.goBack}
-            {...props}
-          />
-        ))
-      }
-      style={{ paddingTop: insets.top }}
-    />
-  );
-};
-
 export default function App() {
   const Stack = createNativeStackNavigator();
+  const Drawer = createDrawerNavigator();
 
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        
-        <Stack.Navigator screenOptions={{header: Header}}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Like" component={LikeScreen} />
-          <Stack.Screen name="Chat" component={ChatScreen} />
-          <Stack.Screen name="Rating" component={RatingScreen} />
-          <Stack.Screen name="TaskList" component={TaskListScreen} />
-        </Stack.Navigator>
+        <Drawer.Navigator initialRouteName='Home' useLegacyImplementation>
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Like" component={LikeScreen} />
+          <Drawer.Screen name="Chat" component={ChatScreen} />
+          <Drawer.Screen name="Rating" component={RatingScreen} />
+          <Drawer.Screen name="TaskList" component={TaskListScreen} />
+        </Drawer.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
